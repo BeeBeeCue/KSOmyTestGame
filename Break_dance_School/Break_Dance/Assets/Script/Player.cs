d7db1ddaf69c597889b98e5c88e2ebe51e05569d
ASD <ngocson.bui0312@gmail.com>
2018-02-06 02:03:45 +0200

    Some Asset and a test field - reup

---
+++ Break_dance_School/Break_Dance/Assets/Script/Player.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+
+namespace AssemblyCSharp
+{
+	
+	public class Player: MonoBehaviour
+	{
+		public Player ()
+		{
+			
+		}
+
+		public void Move(string input)
+		{
+			switch (input) 
+			{
+			case "up":
+				transform.position = new Vector2 (transform.position.x, (float)(transform.position.y+0.643));
+				break;
+			case "left":
+				transform.position = new Vector2 ((float)(transform.position.x-0.643), transform.position.y);
+				break;
+			case "right":
+				transform.position = new Vector2 ((float)(transform.position.x+0.643), transform.position.y);
+				break;
+			case "down":
+				transform.position = new Vector2 (transform.position.x,(float)( transform.position.y-0.643));
+				break;
+			}
+
+		}
+		void Update()
+		{
+			if (Input.GetKeyDown (KeyCode.A)) 
+			{
+				this.Move("left");
+			}
+			else if(Input.GetKeyDown(KeyCode.S))
+			{
+				this.Move("down");
+			}
+			else if(Input.GetKeyDown(KeyCode.D))
+			{
+				this.Move("right");
+			}
+			else if(Input.GetKeyDown(KeyCode.W))
+			{
+				this.Move("up");
+			}
+		}
+
+
+
+	}
+}
+
---
+++ Break_dance_School/Break_Dance/Assets/Script/Tile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace AssemblyCSharp
+{
+	public class Tile
+	{
+		private Transform position;
+		public Tile ()
+		{
+		}
+	}
+}
+
